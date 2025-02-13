950394d21b8fe20180d929bb968fac6f06d76685
Brandon Casamichana <brandoncasawork8@gmail.com>
2025-02-13 10:42:24 -0500

    added mui

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,11 @@ import outputs from "../amplify_outputs.json";
 
 import "@aws-amplify/ui-react/styles.css";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
