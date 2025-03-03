a9fc608a0536261efd897ffa76c964efa73a3b31
Brandon Casamichana <brandoncasawork8@gmail.com>
2025-03-03 10:22:12 -0500

    fixed routing

--- src/components/BottomNav.comp.tsx
+++ src/components/BottomNav.comp.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Box, Button, Paper, Typography, IconButton, ButtonProps, CSSObject, IconButtonProps } from "@mui/material";
 import AppIcon from "@mui/icons-material/Apps";
 import GroupsIcon from "@mui/icons-material/Groups";
+import HomeIcon from "@mui/icons-material/HomeRounded";
 import StyledIconButton from "./StyledIconButton.comp";
 import { useNavigate } from "react-router";
 
@@ -12,11 +13,21 @@ function BottomNav() {
 
 	return (
 		<Box sx={{ height: "4rem", paddingLeft: 2, paddingRight: 2, paddingBottom: "0.5rem", display: "flex", flexDirection: "row", gap: "1rem", justifyContent: "center", alignItems: "center" }}>
-			<StyledIconButton sx={{ width: "2.625rem", height: "2.625rem", borderRadius: "0.75rem" }}>
+			<StyledIconButton
+				sx={{ width: "2.625rem", height: "2.625rem", borderRadius: "0.75rem" }}
+				onClick={() => {
+					navigate("/");
+				}}
+			>
 				<AppIcon />
 			</StyledIconButton>
-			<StyledIconButton sx={{ width: "3.5rem", height: "3.5rem", borderRadius: "1rem" }}>
-				<AppIcon sx={{ fontSize: "1.75em" }} />
+			<StyledIconButton
+				sx={{ width: "3.5rem", height: "3.5rem", borderRadius: "1rem" }}
+				onClick={() => {
+					navigate("/");
+				}}
+			>
+				<HomeIcon sx={{ fontSize: "1.75em" }} />
 			</StyledIconButton>
 			<StyledIconButton
 				sx={{ width: "2.625rem", height: "2.625rem", borderRadius: "0.75rem" }}
